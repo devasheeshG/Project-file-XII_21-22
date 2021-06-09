9ef06a09c36197a2a1a13305a745578c8cbf3578
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2021-06-09 21:33:21 +0530

    added sol.

--- Q5.py
+++ Q5.py
@@ -1,1 +1,22 @@
 # Q5. Write a program to input a number and test if it is a prime number or not.
+
+num = 29
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
--- Q6.py
+++ Q6.py
@@ -1,14 +1,17 @@
 #  Q6. Write a programs to print the following pattern :
- a)A
-   A B
-   A B C
-   A B C D
-   A B C D E
- b)1
-   1 2
-   1 2 3
-   1 2 3 4
- c) 4 3 2 1
-    3 2 1
-    2 1
-    1
+'''     a)A
+          A B
+          A B C
+          A B C D
+          A B C D E
+        b)1
+          1 2
+          1 2 3
+          1 2 3 4
+        
+        c) 4 3 2 1
+            3 2 1
+            2 1
+            1       '''
+
+            
